367bde8cb01b7cba656caf0aefedfba43261b8f7
Aleksandr Momziakov <73888668+Sasanano@users.noreply.github.com>
2021-03-16 11:44:24 +0300

    Modal 1.1

--- js/script.js
+++ js/script.js
@@ -96,14 +96,18 @@ window.addEventListener('DOMContentLoaded', () => {
           modalCloseBtn = document.querySelector('[data-close]')
 
     modalTrigger.forEach(btn => {
-        btn.addEventListener('click', () => {
-            modal.style.display = 'block';
-            document.body.style.overflow = 'hidden';
-        })
+        btn.addEventListener('click', openModal)
     });
     
-    modalCloseBtn.addEventListener('click', () => {
+    function openModal() {
+        modal.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+    }
+
+    function closeModal() {
         modal.style.display = 'none';
         document.body.style.overflow = '';
-    })
+    }
+
+    modalCloseBtn.addEventListener('click', closeModal)
 });
