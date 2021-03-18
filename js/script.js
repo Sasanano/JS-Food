3cffb89ee710b5d48e5330692b5274a0105eeffe
Aleksandr Momziakov <73888668+Sasanano@users.noreply.github.com>
2021-03-18 10:56:36 +0300

    Number of current slide is shown into slider

--- js/script.js
+++ js/script.js
@@ -238,10 +238,17 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const slides = document.querySelectorAll('.offer__slide'),
           prev = document.querySelector('.offer__slider-prev'),
-          next = document.querySelector('.offer__slider-next');
+          next = document.querySelector('.offer__slider-next'),
+          current = document.querySelector('#current'),
+          total = document.querySelector('#total');
 
     let slideIndex = 1;
     showSlides(slideIndex);
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+    } else {
+        total.textContent = slides.length;
+    };
 
     function showSlides(n) {
         if (n > slides.length) {
@@ -253,7 +260,12 @@ window.addEventListener('DOMContentLoaded', () => {
         }
         slides.forEach(item => item.style.display = 'none');
         slides[slideIndex - 1].style.display = 'block';
-    }
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        };
+    };
 
     function plusSlides(n) {
         showSlides(slideIndex += n)
