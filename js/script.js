18d4e5178b85d3e8ffee407a9eeddba07e3c9e01
Aleksandr Momziakov <73888668+Sasanano@users.noreply.github.com>
2021-03-18 10:40:44 +0300

    Slider added

--- js/script.js
+++ js/script.js
@@ -217,7 +217,7 @@ window.addEventListener('DOMContentLoaded', () => {
                 method: "POST",
                 headers: {
                     'Content-type': 'application/json'
-                }
+                },
                 body: JSON.stringify(object)
             }).then(data => {
                 console.log(data);
@@ -232,5 +232,38 @@ window.addEventListener('DOMContentLoaded', () => {
                 form.reset();
             });
         });
+    };
+
+    //Slider
+
+    const slides = document.querySelectorAll('.offer__slide'),
+          prev = document.querySelector('.offer__slider-prev'),
+          next = document.querySelector('.offer__slider-next');
+
+    let slideIndex = 1;
+    showSlides(slideIndex);
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+        slides.forEach(item => item.style.display = 'none');
+        slides[slideIndex - 1].style.display = 'block';
     }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n)
+    };
+
+    prev.addEventListener('click', () => {
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', () => {
+        plusSlides(1);
+    });
 });
